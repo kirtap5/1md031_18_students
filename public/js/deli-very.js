63e5fa859c369438adbab449bc4efda0e3251dcf
kirtap5 <patrik.gerdelius@hotmail.com>
2020-11-20 07:45:53 +0100

    tempData

--- public/js/deli-very.js
+++ public/js/deli-very.js
@@ -8,6 +8,12 @@ var vm = new Vue({
   el: '#dots',
   data: {
     orders: {},
+    tempTarget: {
+      details: {
+        x: 0,
+        y: 0,
+      }
+    }
   },
   created: function () {
     socket.on('initialize', function (data) {
@@ -25,14 +31,34 @@ var vm = new Vue({
       }, 0);
       return lastOrder + 1;
     },
+    displayOrder: function (event) {
+      var offset = {
+        x: event.currentTarget.getBoundingClientRect().left,
+        y: event.currentTarget.getBoundingClientRect().top
+      };
+
+      this.tempTarget.details.x = event.clientX - 10 - offset.x,
+      this.tempTarget.details.y = event.clientY - 10 - offset.y
+    },
+
+
     addOrder: function (event) {
-      var offset = {x: event.currentTarget.getBoundingClientRect().left,
-                    y: event.currentTarget.getBoundingClientRect().top};
-      socket.emit("addOrder", { orderId: this.getNext(),
-                                details: { x: event.clientX - 10 - offset.x,
-                                           y: event.clientY - 10 - offset.y },
-                                orderItems: ["Beans", "Curry"]
-                              });
+      var offset = {
+        x: event.currentTarget.getBoundingClientRect().left,
+        y: event.currentTarget.getBoundingClientRect().top
+      };
+      socket.emit("addOrder", {
+        // orderId: this.getNext(),
+        orderId: this.getNext(),
+        details: {
+          x: event.clientX - 10 - offset.x,
+          y: event.clientY - 10 - offset.y
+        },
+        orderItems: ["Beans", "Curry"]
+      });
     }
   }
 });
+
+
+//console.log(menuInstance.$data.burgers[0].name);
